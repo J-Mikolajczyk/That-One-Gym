3ac9d818d2791d65de179d72d18f970e7c13de39
John Mikolajczyk <johnpaulmik@gmail.com>
2025-07-12 13:54:44 -0400

    Better checks and xs

--- app/gyms/[id]/page.tsx
+++ app/gyms/[id]/page.tsx
@@ -4,11 +4,16 @@ import { notFound } from "next/navigation";
 function Feature({ available, label }: { available: boolean; label: string }) {
   return (
     <p className="flex items-center gap-2">
-      <span
-        className={available ? "text-green-600" : "text-red-600"}
-        style={{ fontFamily: "Arial, sans-serif" }} // prevent emoji font
-      >
-        {available ? "✔" : "✘"}
+      <span className={available ? "text-green-500" : "text-red-500"}>
+        {available ? (
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg>
+        ) : (
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6" >
+            <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg>
+        )}
       </span>
       {label}
     </p>
